1ca725587944074955f8db109ff9526c0440f8b4
Neutron-Pro <neutron@neutronstars.fr>
2020-11-16 01:04:28 +0100

    Refont of pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,8 +74,7 @@ function loadHn(page, line) {
 /** @param {PageGenerator} page
  * @param {string} line */
 function loadElement(page, line) {
-    console.log(line);
-    const tab = line.substring(1, line.length - 2).trim().split('#');
+    const tab = line.substring(1, line.length - 1).trim().split('#');
     if (tab.length === 0) { return }
     page.app += '<' + (tab[0].startsWith('un') ? ('/' + tab[0].substring(2, tab[0].length)) : tab[0])
     let id = '#';
